28dc87815d6485270723106b5b86663e45806bbc
mohamedhgagy <m.hagagy@laplacesoftware.com>
2023-12-12 21:57:53 +0200

    [ADD]md_report_preview

--- md_report_preview/controllers/preview_invoice.py
+++ md_report_preview/controllers/preview_invoice.py
@@ -2,7 +2,7 @@
 from odoo import http
 from odoo.http import request
 
-
+import json
 class PreviewInvoiceController(http.Controller):
 
     @http.route('/api/mdsa/preview/invoice/<model("account.move"):invoice>', type='json', auth='user')
@@ -11,20 +11,26 @@ class PreviewInvoiceController(http.Controller):
         if invoice:
             # Generate the URL to preview the invoice
             preview_url = invoice.get_portal_url()
-
+            host_url = request.httprequest.host_url[:-1] if request.httprequest.host_url else ''
             # Construct JSON response
             response = {
                 'invoice_id': invoice.id,
-                'preview_url': preview_url,
+                'preview_url': host_url+preview_url,
             }
 
             return response, 200
 
         return {'error': 'Invoice not found'}, 404
 
-    @http.route('/api/mdsa/get/invoice/<model("sale.order"):sale_order>', type='json', auth='user')
-    def get_invoice(self, sale_order, **kwargs):
-        if sale_order.sudo().invoice_ids:
-            invoice_urls = [self.preview_invoice(inv) for inv in sale_order.invoice_ids]
-            return invoice_urls
-        return {'error': 'Invoices not found'}, 404
+    @http.route('/api/mdsa/get/invoices', type='json', auth='user', method=['POST'])
+    def get_invoice(self):
+        error = {'error': 'Invoices not found'}, 404
+        order_dict = json.loads(request.httprequest.data)
+        order_name = order_dict.get('order_name', False)
+        if order_name:
+            sale_order = request.env['sale.order'].sudo().search([('name', '=', order_name)], limit=1)
+            if sale_order and sale_order.sudo().invoice_ids:
+                invoice_urls = [self.preview_invoice(inv)[0] for inv in sale_order.invoice_ids]
+                return invoice_urls, 200
+            return error, 400
+        return error, 400
